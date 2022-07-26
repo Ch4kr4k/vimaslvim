a764b5d5a2242e2dcbe102a20ad4b018ced97cf6
BL4CK-R34P3R <32250841+BL4CK-R34P3R@users.noreply.github.com>
2022-07-26 04:55:05 -0400

    install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+rm -rf ~/vimrc
+rm -rf ~/.vim
+
+
+
+curl -fLo ~/.vimrc --create-dirs \
+  https://raw.githubusercontent.com/BL4CK-R34P3R/vimaslvim/main/.vimrc
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -fLo ~/.vim/rc/dein.toml --create-dirs \
+  https://raw.githubusercontent.com/BL4CK-R34P3R/vimaslvim/main/dein.toml
+
+curl -fLo ~/.vim/rc/dein_lazy.toml --create-dirs \
+  https://github.com/BL4CK-R34P3R/vimaslvim/blob/main/dein_lazy.toml
+
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+
+  sh ./installer.sh ~/.vim/dein
+
+  vim
