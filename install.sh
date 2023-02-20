da50556cf67642bb8a1ddc50dbdd364efdb75f4b
Ch4kr4k <32250841+Ch4kr4k@users.noreply.github.com>
2023-02-20 21:20:39 +0530

    Update

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@ rm -rf ~/.vim
 
 
 curl -fLo ~/.vimrc --create-dirs \
-  https://raw.githubusercontent.com/BL4CK-R34P3R/vimaslvim/main/.vimrc
+  https://raw.githubusercontent.com/Ch4kr4k/vimaslvim/main/.vimrc
   
 curl -fLo ~/.vim/colors/NeoSolarized.vim --create-dirs \
-  https://raw.githubusercontent.com/BL4CK-R34P3R/vimaslvim/main/NeoSolarized.vim
+  https://raw.githubusercontent.com/Ch4kr4k/vimaslvim/main/NeoSolarized.vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 curl -fLo ~/.vim/rc/dein.toml --create-dirs \
-  https://raw.githubusercontent.com/BL4CK-R34P3R/vimaslvim/main/dein.toml
+  https://raw.githubusercontent.com/Ch4kr4k/vimaslvim/main/dein.toml
 
 touch ~/.vim/rc/dein_lazy.toml
 
